b070af9db47f3701da7cb41aff7d9e1b23d9e253
Yosh de Vos <yosh@insiders.nl>
2014-12-09 17:18:09 +0100

    Passwordless rsync and cleaning up code

--- cap-drupal-multisite.gemspec
+++ cap-drupal-multisite.gemspec
@@ -1,21 +1,23 @@
 # -*- encoding: utf-8 -*-
 $:.push File.expand_path("../lib", __FILE__)
 
-Gem::Specification.new do |s|
-  s.name        = "cap-drupal-multisite"
-  s.version     = "0.1.0"
-  s.authors     = ["Yosh de Vos"]
-  s.email       = ["yosh@elzorro.nl"]
-  s.homepage    = "https://github.com/yoshz/cap-drupal-multisite"
-  s.summary     = "A collection of capistrano tasks for deploying drupal sites"
-  s.description = "A collection of capistrano tasks for deploying drupal sites"
-  s.license     = 'MIT'
-  #s.rubyforge_project = "capistrano-drupal"
+Gem::Specification.new do |spec|
+  spec.name        = "cap-drupal-multisite"
+  spec.version     = "0.1.0"
+  spec.authors     = ["Yosh de Vos"]
+  spec.email       = ["yosh@elzorro.nl"]
+  spec.homepage    = "https://github.com/yoshz/cap-drupal-multisite"
+  spec.summary     = "A collection of capistrano tasks for deploying drupal sites"
+  spec.description = "A collection of capistrano tasks for deploying drupal sites"
+  spec.license     = 'MIT'
 
-  s.files         = `git ls-files`.split("\n")
-  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
-  s.require_paths = ["lib"]
+  spec.files         = `git ls-files`.split("\n")
+  spec.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  spec.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  spec.require_paths = ["lib"]
 
-  s.add_runtime_dependency "capistrano", ">= 3.0.0"
+  spec.add_dependency "capistrano", ">= 3.2.1"
+  spec.add_dependency "sshkit", ">= 1.4.0"
+  spec.add_dependency "colorize"
+  spec.add_dependency "highline"
 end
--- lib/cap-drupal-multisite/drupal.rb
+++ lib/cap-drupal-multisite/drupal.rb
@@ -15,9 +15,9 @@ namespace :drupal do
   task :symlink do
     on roles(:all) do
       fetch(:drupal_sites).each do |site|
-        execute "mkdir", "-p #{shared_path}/#{site}/files #{shared_path}/#{site}/private"
+        execute "mkdir", "-p #{shared_path}/#{site}/files #{shared_path}/#{site}/private #{release_path}/#{fetch(:drupal_path)}/sites/#{site}"
         execute "chmod", "2775 #{shared_path}/#{site}/files #{shared_path}/#{site}/private"
-        execute "chgrp", "-R #{fetch(:drupal_group)} #{shared_path}/#{site}/files #{shared_path}/#{site}/private"
+        execute "chgrp", "#{fetch(:drupal_group)} #{shared_path}/#{site}/files #{shared_path}/#{site}/private"
         execute "ln", "-nfs #{shared_path}/#{site}/files #{release_path}/#{fetch(:drupal_path)}/sites/#{site}/files"
         execute "ln", "-nfs #{shared_path}/#{site}/private #{release_path}/#{fetch(:drupal_path)}/sites/#{site}/private"
         #execute :drush, "-l #{site} -r #{release_path}/#{fetch(:drupal_path)} vset --yes file_directory_path sites/#{site}/files"
@@ -38,10 +38,8 @@ namespace :drupal do
   desc "Apply any database updates required"
   task :updatedb do
     on roles(:all) do
-      within "#{release_path}/#{fetch(:drupal_path)}" do
-        fetch(:drupal_sites).each do |site|
-          execute :drush, "-l #{site} -y updatedb"
-        end
+      fetch(:drupal_sites).each do |site|
+        execute :drush, "-l #{site} -r #{release_path}/#{fetch(:drupal_path)} -y updatedb"
       end
     end
   end
@@ -49,10 +47,8 @@ namespace :drupal do
   desc "Revert all features"
   task :feature_revert do
     on roles(:all) do
-      within "#{release_path}/#{fetch(:drupal_path)}" do
-        fetch(:drupal_sites).each do |site|
-          execute :drush, "-l #{site} -y features-revert-all"
-        end
+      fetch(:drupal_sites).each do |site|
+        execute :drush, "-l #{site} -r #{release_path}/#{fetch(:drupal_path)} -y features-revert-all"
       end
     end
   end
@@ -62,7 +58,7 @@ namespace :drupal do
     on roles(:all) do
       within "#{release_path}/#{fetch(:drupal_path)}" do
         fetch(:drupal_sites).each do |site|
-          execute :drush, "-l #{site} -y cache-clear all"
+          execute :drush, "-l #{site} -r #{release_path}/#{fetch(:drupal_path)} -y cache-clear all"
         end
       end
     end
@@ -73,10 +69,8 @@ namespace :drupal do
     desc 'Dump database for each site to <remote>:release_path'
     task :dump do 
       on roles(:all) do
-        within "#{release_path}/#{fetch(:drupal_path)}" do
-          fetch(:drupal_sites).each do |site|
-            execute :drush, "-l #{site} sql-dump | gzip > #{release_path}/#{site}.sql.gz"
-          end
+        fetch(:drupal_sites).each do |site|
+          execute :drush, "-l #{site} -r #{release_path}/#{fetch(:drupal_path)} sql-dump | gzip > #{release_path}/#{site}.sql.gz"
         end
       end
     end
@@ -84,9 +78,11 @@ namespace :drupal do
     desc 'Restore database for each site from dump in <remote>:release_path'
     task :restore do 
       on roles(:all) do
-        within "#{release_path}/#{fetch(:drupal_path)}" do
-          fetch(:drupal_sites).each do |site|
-            execute "zcat #{release_path}/#{site}.sql.gz", :drush, "-l #{site} -q sql-connect"
+        fetch(:drupal_sites).each do |site|
+          if test("[ -f #{release_path}/#{site}.sql.gz ]")
+            execute "zcat #{release_path}/#{site}.sql.gz | `drush -l #{site} -r #{release_path}/#{fetch(:drupal_path)} sql-connect`"
+          else
+            raise "Database dump for #{site} is not available"
           end
         end
       end
@@ -95,10 +91,8 @@ namespace :drupal do
     desc 'Download database dumps from <remote>:release_path to <local>:db/'
     task :download do 
       on roles(:all) do
-        within "#{release_path}/#{fetch(:drupal_path)}" do
-          fetch(:drupal_sites).each do |site|
-            download! "#{release_path}/#{site}.sql.gz", "db/"
-          end
+        fetch(:drupal_sites).each do |site|
+          download! "#{release_path}/#{site}.sql.gz", "db/"
         end
       end
     end
@@ -106,10 +100,8 @@ namespace :drupal do
     desc 'Upload database dumps from <local>:db/ to <remote>:release_path'
     task :upload do 
       on roles(:all) do
-        within "#{release_path}/#{fetch(:drupal_path)}" do
-          fetch(:drupal_sites).each do |site|
-            upload! "db/#{site}.sql.gz", "#{release_path}/#{site}.sql.gz"
-          end
+        fetch(:drupal_sites).each do |site|
+          upload! "db/#{site}.sql.gz", "#{release_path}/#{site}.sql.gz"
         end
       end
     end
@@ -122,8 +114,8 @@ namespace :drupal do
       on roles(:all) do |server|
         fetch(:drupal_sites).each do |site|
           puts "Downloading files of #{site} from #{server}"
-          system("rsync -a --del -L -K --rsh='ssh -p #{server.port}' #{server.user}@#{server.hostname}:#{shared_path}/#{site}/files/ files/#{site}")
-          system("rsync -a --del -L -K --rsh='ssh -p #{server.port}' #{server.user}@#{server.hostname}:#{shared_path}/#{site}/private/ private/#{site}")
+          system("sshpass -p#{server.password} rsync -a --del -L -K --rsh='ssh -p #{server.port}' #{server.user}@#{server.hostname}:#{shared_path}/#{site}/files/ files/#{site}")
+          system("sshpass -p#{server.password} rsync -a --del -L -K --rsh='ssh -p #{server.port}' #{server.user}@#{server.hostname}:#{shared_path}/#{site}/private/ private/#{site}")
         end
       end
     end
@@ -133,8 +125,8 @@ namespace :drupal do
       on roles(:all) do |server|
         fetch(:drupal_sites).each do |site|
           puts "Uploading files of #{site} to #{server}"
-          system("rsync -a --del -L -K --rsh='ssh -p #{server.port}' files/#{site}/ #{server.user}@#{server.hostname}:#{shared_path}/#{site}/files")
-          system("rsync -a --del -L -K --rsh='ssh -p #{server.port}' private/#{site}/ #{server.user}@#{server.hostname}:#{shared_path}/#{site}/private")
+          system("sshpass -p#{server.password} rsync -a --del -L -K --rsh='ssh -p #{server.port}' files/#{site}/ #{server.user}@#{server.hostname}:#{shared_path}/#{site}/files")
+          system("sshpass -p#{server.password} rsync -a --del -L -K --rsh='ssh -p #{server.port}' private/#{site}/ #{server.user}@#{server.hostname}:#{shared_path}/#{site}/private")
         end
       end
     end
